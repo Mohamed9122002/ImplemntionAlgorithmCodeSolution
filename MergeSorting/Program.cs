992fb91988e9e6b6f5e4349f0fcc9695db141046
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-06-12 12:23:59 +0300

    Segregate Positive and Negative Numbers

--- MergeSorting/Program.cs
+++ MergeSorting/Program.cs
@@ -9,7 +9,7 @@
             // exit if end <= start 
             if (end <= start) return;
             // Calculate the midpoint
-            int midpoint = (end + start ) / 2;
+            int midpoint = (end + start) / 2;
             // Recursively sort the left half
             MergeSort(array, start, midpoint);
             // Recursively sort the right half
@@ -44,7 +44,7 @@
             // Compare And Sort 
             while (i < left_length && j < right_length)
             {
-                if(leftArray[i] <= rightArray[j])
+                if (leftArray[i] <= rightArray[j])
                 {
                     array[k] = leftArray[i];
                     i++;
@@ -75,14 +75,80 @@
             }
 
         }
+        #region Segregate Positive and Negative Numbers
+        public static void Segreate(int[] array, int start, int end)
+        {
+            if (end <= start) return;
+            int midpoint = (end + start) / 2;
+            // Recursively sort the left half 
+            Segreate(array, start, midpoint);
+            // Recursively sort the right half
+            Segreate(array, midpoint + 1, end);
+            // Merge the sorted halves
+            MergeSegregate(array, start, midpoint, end);
+        }
+        public static void MergeSegregate(int[] array, int start, int midpoint, int end)
+        {
+            int i, j, k;
+            int left_length = midpoint - start + 1;
+            int right_length = end - midpoint;
+            int[] leftArray = new int[left_length];
+            int[] rightArray = new int[right_length];
+            // Fill the left array 
+            for (i = 0; i < left_length; i++)
+            {
+                leftArray[i] = array[start + i];
+            }
+            // Fill the right array
+            for (j = 0; j < right_length; j++)
+            {
+                rightArray[j] = array[midpoint + 1 + j];
+            }
+            // Initialize indices
+            i = 0; // Index for left array
+            j = 0; // Index for right array
+            k = start; // Index for merged array
+            // Compare And Sort
+            while (i < left_length && leftArray[i] <= 0)
+            {
+                array[k] = leftArray[i];
+                i++;
+                k++;
+            }
+            while (j < right_length && rightArray[j] <= 0)
+            {
+                array[k] = rightArray[j];
+                j++;
+                k++;
+            }
+            while (i < left_length)
+            {
+                array[k] = leftArray[i];
+                i++;
+                k++;
+            }
+            while (j < right_length)
+            {
+                array[k] = rightArray[j];
+                j++;
+                k++;
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Merge Sorting ");
-            int[] array = { 8, 65, 9, 7, 3, 5, 54 };
+            //Console.WriteLine("Hello, World! Merge Sorting ");
+            //int[] array = { 8, 65, 9, 7, 3, 5, 54 };
 
+            //Console.WriteLine(String.Join(", ", array));
+            //MergeSort(array, 0, array.Length - 1);
+            //Console.WriteLine(String.Join(", ", array));
+            #region Segregate Positive and Negative Numbers
+            int[] array = { 6, -5, 12, 10, -9, -1 };
             Console.WriteLine(String.Join(", ", array));
-            MergeSort(array, 0, array.Length - 1);
+            Segreate(array, 0, array.Length - 1);
             Console.WriteLine(String.Join(", ", array));
+            #endregion
         }
     }
 }
